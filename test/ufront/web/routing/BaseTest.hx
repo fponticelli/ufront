d932019f1f5b81b2d20c2c1c23dfb87f784f7510
franco ponticelli <franco.ponticelli@gmail.com>
2012-03-11 18:40:56 -0600

    fix

--- test/ufront/web/routing/BaseTest.hx
+++ test/ufront/web/routing/BaseTest.hx
@@ -6,22 +6,22 @@ import ufront.web.mvc.MvcRouteHandler;
 
 class BaseTest
 {
-	public function new(){} 
-	
+	public function new(){}
+
 	public function createRoute(url : String, ?constraint : IRouteConstraint, ?constraints : Array<IRouteConstraint>)
-	{                                                                                                                
+	{
 		if(null == constraints)
 			constraints = [];
 		if(null != constraint)
 			constraints.push(constraint);
 		return new Route(url, new MvcRouteHandler(), constraints);
 	}
-	
-	public function createContext(url : String) : HttpContext 
+
+	public function createContext(url : String) : HttpContext
 	{
 		var request = new HttpRequestMock();
 		request.setUri(url);
 		var context = new HttpContextMock(request);
 		return context;
-	}	
+	}
 }
