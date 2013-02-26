a72dd8e680dfec67e26d4d8e04989d18b4a920e8
Jason O'Neil <jason.oneil@gmail.com>
2013-02-26 12:26:21 +0800

    More getter/setter standardising for Haxe3

--- src/ufront/web/HttpRequest.hx
+++ src/ufront/web/HttpRequest.hx
@@ -13,8 +13,8 @@ import haxe.io.Bytes;
 */
 class HttpRequest
 {
-  public static var instance(getInstance, null) : HttpRequest;
-	static function getInstance() : HttpRequest
+  public static var instance(get, null) : HttpRequest;
+	static function get_instance() : HttpRequest
 	{
 		if(null == instance)
 #if php
@@ -33,79 +33,79 @@ class HttpRequest
 	 * - post values
 	 * - cookies
 	 */
- 	public var params(getParams, null) : CascadeHash<String>;
- 	function getParams()
+ 	public var params(get, null) : CascadeHash<String>;
+ 	function get_params()
  	{
  		if (null == params)
  			params = new CascadeHash([new Hash(), query, post, cookies]);
  		return params;
  	}
 
- 	public var queryString(getQueryString, null) : String;
- 	function getQueryString() return throw new AbstractMethod();
+ 	public var queryString(get, null) : String;
+ 	function get_queryString() return throw new AbstractMethod();
 
- 	public var postString(getPostString, null) : String;
- 	function getPostString() return throw new AbstractMethod();
+ 	public var postString(get, null) : String;
+ 	function get_postString() return throw new AbstractMethod();
 
- 	public var query(getQuery, null) : Hash<String>;
- 	function getQuery() return throw new AbstractMethod();
+ 	public var query(get, null) : Hash<String>;
+ 	function get_query() return throw new AbstractMethod();
 
- 	public var post(getPost, null) : Hash<String>;
- 	function getPost() return throw new AbstractMethod();
+ 	public var post(get, null) : Hash<String>;
+ 	function get_post() return throw new AbstractMethod();
 
- 	public var cookies(getCookies, null) : Hash<String>;
- 	function getCookies() return throw new AbstractMethod();
+ 	public var cookies(get, null) : Hash<String>;
+ 	function get_cookies() return throw new AbstractMethod();
 
- 	public var hostName(getHostName, null) : String;
- 	function getHostName() return throw new AbstractMethod();
+ 	public var hostName(get, null) : String;
+ 	function get_hostName() return throw new AbstractMethod();
 
- 	public var clientIP(getClientIP, null) : String;
- 	function getClientIP() return throw new AbstractMethod();
+ 	public var clientIP(get, null) : String;
+ 	function get_clientIP() return throw new AbstractMethod();
 
- 	public var uri(getUri, null) : String;
- 	function getUri() return throw new AbstractMethod();
+ 	public var uri(get, null) : String;
+ 	function get_uri() return throw new AbstractMethod();
 
- 	public var clientHeaders(getClientHeaders, null) : Hash<String>;
- 	function getClientHeaders() return throw new AbstractMethod();
+ 	public var clientHeaders(get, null) : Hash<String>;
+ 	function get_clientHeaders() return throw new AbstractMethod();
 
-	public var userAgent(getUserAgent, null) : UserAgent;
- 	function getUserAgent() return throw new AbstractMethod();
+	public var userAgent(get, null) : UserAgent;
+ 	function get_userAgent() return throw new AbstractMethod();
 
- 	public var httpMethod(getHttpMethod, null) : String;
- 	function getHttpMethod() return throw new AbstractMethod();
+ 	public var httpMethod(get, null) : String;
+ 	function get_httpMethod() return throw new AbstractMethod();
 
- 	public var scriptDirectory(getScriptDirectory, null) : String;
- 	function getScriptDirectory() return throw new AbstractMethod();
+ 	public var scriptDirectory(get, null) : String;
+ 	function get_scriptDirectory() return throw new AbstractMethod();
 
- 	public var authorization(getAuthorization, null) : { user : String, pass : String };
- 	function getAuthorization() return throw new AbstractMethod();
+ 	public var authorization(get, null) : { user : String, pass : String };
+ 	function get_authorization() return throw new AbstractMethod();
 
  	public function setUploadHandler(handler : IHttpUploadHandler) throw new AbstractMethod();
 
 	// urlReferrrer
-	//public var acceptTypes(getAcceptTypes, null) : Array<String>;
-	//public var sessionId(getSessionId, null) : String;
+	//public var acceptTypes(get, null) : Array<String>;
+	//public var sessionId(get, null) : String;
 
 	/**
 	 * never has trailing slash. If the application is in the server root the path will be emppty ""
 	 */
-	//public var applicationPath(getApplicationPath, null) : String;
-	//public var broswer(getBrowser, setBrowser) : HttpBrowserCapabilities;
-	//public var encoding(getEncoding, setEncoding) : String;
-	//public var contentLength(getContentLength, null) : Int;
-	//public var contentType(getContentType, null) : String;
-	//public var mimeType(getMimeType, setMimeType) : String;
-	//public var files(getFiles, null) : List<HttpPostedFile>;
-	//public var httpMethod(getHttpMethod, null) : String;
-	//public var isAuthenticated(getIsAuthenticated, null) : String;
+	//public var applicationPath(get, null) : String;
+	//public var broswer(get, setBrowser) : HttpBrowserCapabilities;
+	//public var encoding(get, setEncoding) : String;
+	//public var contentLength(get, null) : Int;
+	//public var contentType(get, null) : String;
+	//public var mimeType(get, setMimeType) : String;
+	//public var files(get, null) : List<HttpPostedFile>;
+	//public var httpMethod(get, null) : String;
+	//public var isAuthenticated(get, null) : String;
 	/**
 	 * evaluates to true if the IP address is 127.0.0.1 or the same as the client ip address
 	 */
-	//public var isLocal(getIsLocal, null) : String;
-	//public var isSecure(getIsSecure, null) : String;
+	//public var isLocal(get, null) : String;
+	//public var isSecure(get, null) : String;
 
-	//public var userAgent(getUserAgent, null) : String;
-	//public var userHostAddress(getUserHostAddress, null) : String;
-	//public var userHostName(getUserHostName, null) : String;
+	//public var userAgent(get, null) : String;
+	//public var userHostAddress(get, null) : String;
+	//public var userHostName(get, null) : String;
 	//public var userLanguages(get, null) : Array<String>;
 }
